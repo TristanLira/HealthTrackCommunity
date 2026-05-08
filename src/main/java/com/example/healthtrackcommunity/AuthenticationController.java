f37eddd19f26c0ac3d37c589f6e04777fcaba1e1
Trisss16 <lirt.liraruiztristan.ra7@gmail.com>
2026-05-07 22:08:17 -0600

    feat: cambio de vistas según la cuenta loggeada

--- src/main/java/com/example/healthtrackcommunity/AuthenticationController.java
+++ src/main/java/com/example/healthtrackcommunity/AuthenticationController.java
@@ -6,12 +6,19 @@ import config.DoctorDAO;
 import config.PatientDAO;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+import javax.print.Doc;
+import java.io.IOException;
 
 public class AuthenticationController {
 
@@ -80,7 +87,7 @@ public class AuthenticationController {
         showed.setManaged(true);
     }
 
-    /**********************************registro e inicio de sesión******************************************/
+    /********************************** registro de pacientes y médicos ******************************************/
 
     boolean emptyStr(String str) {
         return str == null || str.isEmpty();
@@ -136,6 +143,80 @@ public class AuthenticationController {
         patientEmailField.clear();
         patientNameField.clear();
         patientPasswordField.clear();
+    }
+
+
+    /********************************** inicio de sesión ******************************************/
+
+    public void login(ActionEvent actionEvent) throws IOException {
+        String email = loginEmailField.getText();
+        String password = loginPasswordField.getText();
+
+        if (emptyStr(email) || emptyStr(password)) {
+            errorAlert("Campos vacíos", "Por favor rellene todos los campos.");
+            return;
+        }
+
+        //busca la cuenta en los médicos
+        Doctor dLogged = null;
+        for (Doctor i: doctors) {
+            if (i.getEmail().equals(email) && i.getPassword().equals(password)) dLogged = i;
+        }
+
+        //busca la cuenta en los pacientes
+        Patient pLogged = null;
+        for(Patient i: patients) {
+            if (i.getEmail().equals(email) && i.getPassword().equals(password)) pLogged = i;
+        }
+
+        if (dLogged == null && pLogged == null) {
+            errorAlert("Cuenta inexistente.", "El email o la contraseña son incorrectos. Por favor inténtelo de nuevo.");
+            return;
+        }
+
+        //decide que vista cargar según la cuenta obtenida
+        if (dLogged != null) {
+            loadDoctorView(actionEvent, dLogged);
+        }
+        else if (pLogged != null) {
+            loadPatientView(actionEvent, pLogged);
+        } else {
+            //TODO cuenta de administrador
+        }
+    }
+
+    private void loadDoctorView(ActionEvent event, Doctor logged) throws IOException {
+        //cargar el fxml
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("doctor-view.fxml"));
+        Parent root = loader.load();
+
+        //obtiene el controlador de la vista desde el loader e inicializa los datos del usuario loggeado
+        DoctorController controller = loader.getController();
+        controller.setLoggedUser(doctorDAO, logged);
+
+        //obtiene el stage donde está el botón que creó el evento
+        Scene currentScene = ((Node) event.getSource()).getScene();
+        Stage stage = (Stage) currentScene.getWindow();
+        Scene newScene = new Scene(root, currentScene.getWidth(), currentScene.getHeight());
+        stage.setScene(newScene);
+        stage.show();
+    }
+
+    private void loadPatientView(ActionEvent event, Patient logged) throws IOException {
+        //cargar el fxml
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("patient-view.fxml"));
+        Parent root = loader.load();
+
+        //obtiene el controlador de la vista desde el loader e inicializa los datos del usuario loggeado
+        PatientController controller = loader.getController();
+        controller.setLoggedUser(patientDAO, logged);
+
+        //obtiene el stage donde está el botón que creó el evento
+        Scene currentScene = ((Node) event.getSource()).getScene();
+        Stage stage = (Stage) currentScene.getWindow();
+        Scene newScene = new Scene(root, currentScene.getWidth(), currentScene.getHeight());
+        stage.setScene(newScene);
+        stage.show();
     }
 
 
