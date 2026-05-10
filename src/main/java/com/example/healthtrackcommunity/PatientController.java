7ae6e965a9aaea54c3670b63cab3619dce001000
TristanLira <24030458@itcelaya.edu.mx>
2026-05-10 02:19:17 -0600

    feat: implementar la escritura de métricas a firebase

--- src/main/java/com/example/healthtrackcommunity/PatientController.java
+++ src/main/java/com/example/healthtrackcommunity/PatientController.java
@@ -7,6 +7,7 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.scene.Node;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
@@ -125,15 +126,119 @@ public class PatientController {
         n.setVisible(true);
     }
 
+
+    /*FRECUENCIA CARDIACA*/
     public void saveHeartRate(ActionEvent actionEvent) {
+        HeartRateMetric h = getHeartRateMetric();
+        if (h == null) {
+            errorAlert("Datos inválidos", "Los datos ingresados no son validos. Por favor ingrese solo números.");
+            return;
+        }
+        heartRateDAO.create(h);
     }
 
+    private HeartRateMetric getHeartRateMetric() {
+        int heartRate;
+
+        try {
+            heartRate = Integer.parseInt(heartRateField.getText().strip());
+        } catch (Exception e) {
+            return null;
+        }
+
+        heartRateField.clear();
+        return new HeartRateMetric(logged.getId(), heartRate);
+    }
+
+
+    /*PRESIÓN ARTERIAL*/
     public void savePressure(ActionEvent actionEvent) {
+        PressureMetric p = getPressureMetric();
+        if (p == null) {
+            errorAlert("Datos inválidos", "Los datos ingresados no son validos. Por favor ingrese solo números.");
+            return;
+        }
+        pressureDAO.create(p);
     }
 
+    private PressureMetric getPressureMetric() {
+        int systolic, diastolic;
+
+        try {
+            systolic = Integer.parseInt(pressureSystolicField.getText().strip());
+            diastolic = Integer.parseInt(pressureDiastolicField.getText().strip());
+        } catch (Exception e) {
+            return null;
+        }
+
+        pressureSystolicField.clear();
+        pressureDiastolicField.clear();
+        return new PressureMetric(logged.getId(), systolic, diastolic);
+    }
+
+
+    /*GLUCOSA*/
     public void saveGlucose(ActionEvent actionEvent) {
+        GlucoseMetric g = getGlucoseMetric();
+        if (g == null) {
+            errorAlert("Datos inválidos", "Los datos ingresados no son validos. Por favor ingrese solo números.");
+            return;
+        }
+        glucoseDAO.create(g);
     }
 
+    private GlucoseMetric getGlucoseMetric() {
+        int glucose;
+
+        try {
+            glucose = Integer.parseInt(glucoseField.getText().strip());
+        } catch (Exception e) {
+            return null;
+        }
+
+        glucoseField.clear();
+        return new GlucoseMetric(logged.getId(), glucose);
+    }
+
+
+    /*PESO/IMC*/
     public void saveWeight(ActionEvent actionEvent) {
+        WeightMetric w = getWeightMetric();
+        if (w == null) {
+            errorAlert("Datos inválidos", "Los datos ingresados no son validos. Por favor ingrese solo números.");
+            return;
+        }
+        weightDAO.create(w);
+    }
+
+    private WeightMetric getWeightMetric() {
+        int weight, height;
+
+        try {
+            weight = Integer.parseInt(weightField.getText().strip());
+            height = Integer.parseInt(heightField.getText().strip());
+        } catch (Exception e) {
+            return null;
+        }
+
+        weightField.clear();
+        heightField.clear();
+        return new WeightMetric(logged.getId(), height, weight);
+    }
+
+    /********************************** ALERTAS ******************************************/
+
+    private void errorAlert(String title, String message) {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle(title);
+        alert.setHeaderText(message);
+        alert.show();
+    }
+
+    private void infoAlert(String title, String message) {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle(title);
+        alert.setHeaderText(message);
+        alert.show();
     }
 }
