e63a878620dd1d8cb09eaadf062fbc25948f8055
TristanLira <24030458@itcelaya.edu.mx>
2026-05-10 01:34:09 -0600

    feat: agregar GUI para el registro de métricas en la vista de paciente

--- src/main/java/com/example/healthtrackcommunity/PatientController.java
+++ src/main/java/com/example/healthtrackcommunity/PatientController.java
@@ -3,9 +3,41 @@ package com.example.healthtrackcommunity;
 import com.example.healthtrackcommunity.models.*;
 import config.MetricDAO;
 import config.PatientDAO;
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 
 public class PatientController {
+
+    /*formulario*/
+
+    public ComboBox<String> metricTypeComboBox;
+    public StackPane metricFormsStackPane;
+
+    public VBox heartRateMetricForm;
+    public TextField heartRateField;
+    public Button saveHeartRateBtn;
+
+    public VBox pressureMetricForm;
+    public TextField pressureSystolicField;
+    public TextField pressureDiastolicField;
+    public Button savePressureBtn;
+    
+    public VBox glucoseMetricForm;
+    public TextField glucoseField;
+    public Button saveGlucoseBtn;
+
+    public VBox weightMetricForm;
+    public TextField weightField;
+    public TextField heightField;
+    public Button saveWeightBtn;
+
 
     //paciente loggeado
     private PatientDAO patientDAO;
@@ -24,6 +56,7 @@ public class PatientController {
     ObservableList<Metric> weight;
 
     public void initialize() {
+        initMetricTypeCombobox();
     }
 
     private void initDAOs() {
@@ -45,4 +78,62 @@ public class PatientController {
         initDAOs();
     }
 
+    /*************************Registro de metricas******************************/
+
+    private void initMetricTypeCombobox() {
+        final String heartRateStr = "Frecuencia cardíaca";
+        final String pressureStr = "Presión arterial";
+        final String glucoseStr = "Glucosa";
+        final String weightStr = "Indice de masa corporal";
+
+        ObservableList<String> comboBoxList = FXCollections.observableArrayList(pressureStr, heartRateStr, glucoseStr, weightStr);
+
+        metricTypeComboBox.setItems(comboBoxList);
+
+        metricTypeComboBox.setOnAction(event -> {
+
+            switch (metricTypeComboBox.getValue()) {
+                case heartRateStr:
+                    showMetricForm(heartRateMetricForm);
+                    break;
+
+                case pressureStr:
+                    showMetricForm(pressureMetricForm);
+                    break;
+
+                case glucoseStr:
+                    showMetricForm(glucoseMetricForm);
+                    break;
+
+                case weightStr:
+                    showMetricForm(weightMetricForm);
+                    break;
+            }
+
+        });
+
+        showMetricForm(pressureMetricForm);
+        metricTypeComboBox.setValue(pressureStr);
+    }
+
+    private void showMetricForm(Node n) {
+        for (Node i: metricFormsStackPane.getChildren()) {
+            i.setManaged(false);
+            i.setVisible(false);
+        }
+        n.setManaged(true);
+        n.setVisible(true);
+    }
+
+    public void saveHeartRate(ActionEvent actionEvent) {
+    }
+
+    public void savePressure(ActionEvent actionEvent) {
+    }
+
+    public void saveGlucose(ActionEvent actionEvent) {
+    }
+
+    public void saveWeight(ActionEvent actionEvent) {
+    }
 }
