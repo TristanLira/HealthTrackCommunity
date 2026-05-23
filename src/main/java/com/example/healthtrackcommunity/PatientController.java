627ebc40870b8e9777c2e56cb5717f67b4600e7c
TristanLira <24030458@itcelaya.edu.mx>
2026-05-23 13:19:36 -0600

    feat: generación de gráficos de evolución

--- src/main/java/com/example/healthtrackcommunity/PatientController.java
+++ src/main/java/com/example/healthtrackcommunity/PatientController.java
@@ -14,16 +14,20 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
 import javafx.scene.control.*;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 public class PatientController {
 
@@ -49,10 +53,11 @@ public class PatientController {
 
     //sección de gráficos
     public VBox chartsSection;
-    public VBox patientGlucoseChart;
-    public VBox patientPressureChart;
-    public VBox patientHeartRateChart;
-    public VBox patientWeightChart;
+    public TabPane chartsTab;
+    public VBox patientGlucoseChartContainer;
+    public VBox patientPressureChartContainer;
+    public VBox patientHeartRateChartContainer;
+    public VBox patientWeightChartContainer;
 
     //seguimiento médico
     public VBox doctorMonitoringSection;
@@ -156,6 +161,59 @@ public class PatientController {
 
         loadMetricDisplays();
         loadRecentDisplays();
+
+        //addMetricsDebug();
+    }
+
+    private void addMetricsDebug() {
+        List<PressureMetric> pressureMetrics = new ArrayList<>();
+        List<GlucoseMetric> glucoseMetrics = new ArrayList<>();
+        List<HeartRateMetric> heartRateMetrics = new ArrayList<>();
+        List<WeightMetric> weightMetrics = new ArrayList<>();
+
+        LocalDate today = LocalDate.now();
+
+        for (int i = 6; i >= 0; i--) {
+            LocalDate day = today.minusDays(i);
+
+            PressureMetric pressureMetric = new PressureMetric(
+                    logged.getId(),
+                    110 + (int)(Math.random() * 20), // sistólica random
+                    70 + (int)(Math.random() * 15)   // diastólica random
+            );
+
+            GlucoseMetric glucoseMetric = new GlucoseMetric(
+                    logged.getId(),
+                    80 + (int)(Math.random() * 60)
+            );
+
+            HeartRateMetric heartRateMetric = new HeartRateMetric(
+                    logged.getId(),
+                    60 + (int)(Math.random() * 41)
+            );
+
+            WeightMetric weightMetric = new WeightMetric(
+                    logged.getId(),
+                    175,
+                    65 + (int)(Math.random() * 10) // peso 65–75 kg
+            );
+
+            // asignar fecha
+            pressureMetric.setDate(day.toString());
+            glucoseMetric.setDate(day.toString());
+            heartRateMetric.setDate(day.toString());
+            weightMetric.setDate(day.toString());
+
+            pressureMetrics.add(pressureMetric);
+            glucoseMetrics.add(glucoseMetric);
+            heartRateMetrics.add(heartRateMetric);
+            weightMetrics.add(weightMetric);
+        }
+
+        for (Metric j: pressureMetrics) pressureDAO.create(j);
+        for (Metric j: glucoseMetrics) glucoseDAO.create(j);
+        for (Metric j: heartRateMetrics) heartRateDAO.create(j);
+        for (Metric j: weightMetrics) weightDAO.create(j);
     }
 
     private void updateRequestInfo() {
@@ -224,11 +282,7 @@ public class PatientController {
         hideAllSections();
         historySection.setVisible(true);
         historySection.setManaged(true);
-
-        Platform.runLater(() -> {
-            historySection.requestLayout();
-            historySection.layout();
-        });
+        reloadTab(historyTab);
     }
 
     @FXML
@@ -236,6 +290,7 @@ public class PatientController {
         hideAllSections();
         chartsSection.setVisible(true);
         chartsSection.setManaged(true);
+        reloadTab(chartsTab);
     }
 
     public void showDoctorMonitoring(ActionEvent actionEvent) {
@@ -434,10 +489,7 @@ public class PatientController {
                         if (i.getClass() != metricClass) continue; //no debería haber otro tipo de métricas en esta lista, pero por si acaso
                         MetricDisplay display = getDisplay(i);
                         display.hideTitle();
-                        Platform.runLater(() -> {
-                            container.getChildren().addFirst(display);
-                            reloadTab(historyTab);
-                        });
+                        Platform.runLater(() -> container.getChildren().addFirst(display));
                         /*Ya que en la base de datos las mediciones se guardan en orden de registro, al leerlas se obtienen primero las más
                          * antiguas. Guardando cada medición recibida de la base de datos al inicio, se terminan mostrando ordenadas en la GUI*/
                     }
@@ -525,7 +577,7 @@ public class PatientController {
 
     private boolean isInLastWeek(LocalDate date) {
         LocalDate lastWeek = LocalDate.now().minusWeeks(1);
-        return date.isAfter(lastWeek) || date.isEqual(lastWeek);
+        return date.isAfter(lastWeek);
     }
 
     private void loadRecentDisplays() {
@@ -552,6 +604,10 @@ public class PatientController {
                         }
                     }
                 }
+
+                //genera el gráfico en otro hilo
+                Thread t = new Thread(() -> generateMetricGraphics());
+                t.start();
             }
         });
     }
@@ -593,6 +649,147 @@ public class PatientController {
         );
 
         showDoctorInfo();
+    }
+
+    /********************************** graficos ******************************************/
+
+    private void generateMetricGraphics() {
+        ObservableList<PressureMetric> pressureMetrics = FXCollections.observableArrayList();
+        ObservableList<GlucoseMetric> glucoseMetrics = FXCollections.observableArrayList();
+        ObservableList<HeartRateMetric> heartRateMetrics = FXCollections.observableArrayList();
+        ObservableList<WeightMetric> bmiMetrics = FXCollections.observableArrayList();
+
+        for (Metric i: recent) {
+            if (i instanceof PressureMetric) pressureMetrics.add((PressureMetric) i);
+            else if (i instanceof GlucoseMetric) glucoseMetrics.add((GlucoseMetric) i);
+            else if (i instanceof HeartRateMetric) heartRateMetrics.add((HeartRateMetric) i);
+            else if (i instanceof WeightMetric) bmiMetrics.add((WeightMetric) i);
+        }
+
+        LineChart<String, Number> pressure = getPressureChart(pressureMetrics);
+        LineChart<String, Number> glucose = getGlucoseChart(glucoseMetrics);
+        LineChart<String, Number> heartRate = getHeartRateChart(heartRateMetrics);
+        LineChart<String, Number> bmi = getBmiChart(bmiMetrics);
+
+        Platform.runLater(() -> {
+            patientPressureChartContainer.getChildren().clear();
+            patientPressureChartContainer.getChildren().add(pressure);
+
+            patientGlucoseChartContainer.getChildren().clear();
+            patientGlucoseChartContainer.getChildren().add(glucose);
+
+            patientHeartRateChartContainer.getChildren().clear();
+            patientHeartRateChartContainer.getChildren().add(heartRate);
+
+            patientWeightChartContainer.getChildren().clear();
+            patientWeightChartContainer.getChildren().add(bmi);
+
+        });
+    }
+
+    private String getDayName(LocalDate date) {
+        switch (date.getDayOfWeek()) {
+            case MONDAY -> {
+                return "LUN";
+            }
+            case TUESDAY -> {
+                return "MAR";
+            }
+            case WEDNESDAY -> {
+                return "MIE";
+            }
+            case THURSDAY -> {
+                return "JUE";
+            }
+            case FRIDAY -> {
+                return "VIE";
+            }
+            case SATURDAY -> {
+                return "SAB";
+            }
+            case SUNDAY -> {
+                return "DOM";
+            }
+
+            default -> {
+                return "";
+            }
+        }
+    }
+
+    private LineChart<String, Number> getPressureChart(ObservableList<PressureMetric> pressureMetrics) {
+        CategoryAxis xAxis = new CategoryAxis();
+        NumberAxis yAxis = new NumberAxis();
+
+        LineChart<String,Number> chart = new LineChart<String,Number>(xAxis,yAxis);
+
+        XYChart.Series sys = new XYChart.Series();
+        sys.setName("Presión sistólica");
+        XYChart.Series dia = new XYChart.Series();
+        dia.setName("Presión diastólica");
+
+        for (PressureMetric i: pressureMetrics) {
+            sys.getData().add(new XYChart.Data(getDayName(i.getDateObj()), i.getSystolic()));
+            dia.getData().add(new XYChart.Data(getDayName(i.getDateObj()), i.getDiastolic()));
+        }
+
+        chart.getData().addAll(sys, dia);
+
+        return chart;
+    }
+
+    private LineChart<String, Number> getGlucoseChart(ObservableList<GlucoseMetric> glucoseMetrics) {
+        CategoryAxis xAxis = new CategoryAxis();
+        NumberAxis yAxis = new NumberAxis();
+
+        LineChart<String,Number> chart = new LineChart<String,Number>(xAxis,yAxis);
+
+        XYChart.Series series = new XYChart.Series();
+        series.setName("Glucosa");
+
+        for (GlucoseMetric i: glucoseMetrics) {
+            series.getData().add(new XYChart.Data(getDayName(i.getDateObj()), i.getGlucose()));
+        }
+
+        chart.getData().add(series);
+
+        return chart;
+    }
+
+    private LineChart<String, Number> getHeartRateChart(ObservableList<HeartRateMetric> heartRateMetrics) {
+        CategoryAxis xAxis = new CategoryAxis();
+        NumberAxis yAxis = new NumberAxis();
+
+        LineChart<String,Number> chart = new LineChart<String,Number>(xAxis,yAxis);
+
+        XYChart.Series series = new XYChart.Series();
+        series.setName("Frecuencia cardíaca");
+
+        for (HeartRateMetric i: heartRateMetrics) {
+            series.getData().add(new XYChart.Data(getDayName(i.getDateObj()), i.getHeartRate()));
+        }
+
+        chart.getData().add(series);
+
+        return chart;
+    }
+
+    private LineChart<String, Number> getBmiChart(ObservableList<WeightMetric> bmiMetrics) {
+        CategoryAxis xAxis = new CategoryAxis();
+        NumberAxis yAxis = new NumberAxis();
+
+        LineChart<String,Number> chart = new LineChart<String,Number>(xAxis,yAxis);
+
+        XYChart.Series series = new XYChart.Series();
+        series.setName("Indice de masa corporal");
+
+        for (WeightMetric i: bmiMetrics) {
+            series.getData().add(new XYChart.Data(getDayName(i.getDateObj()), i.getBmi()));
+        }
+
+        chart.getData().add(series);
+
+        return chart;
     }
 
 
